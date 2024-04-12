c7ef06a2da741144f945f7f2623a047e917ee753
Deathonyou <158500977+Deathonyou@users.noreply.github.com>
2024-04-12 09:33:51 -0500

    Update hex.js
    math to make the hexagons

--- js/hex.js
+++ js/hex.js
@@ -1,9 +1,27 @@
-S(document).ready(function(){
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-	// Create an object attached to a DOM element with the ID "hexmap-1"
-	var hexmap = S.hexmap('hexmap-1');
+const a = 2 * Math.PI / 6;
+const r = 10;
 
-	// Position the hexes and scale them to the container
-	hexmap.positionHexes().resize();
+function init() {
+  drawGrid(canvas.width, canvas.height);
+}
+init();
 
-});
+function drawGrid(width, height) {
+  for (let y = r, j = 0; y + r * Math.sin(a) < height; y += 2 ** ((j + 1) % 2) * r * Math.sin(a), j = 0) {
+    for (let x = r; x + r * (1 + Math.cos(a)) < width; x += r * (1 + Math.cos(a)), y += (-1) ** j++ * r * Math.sin(a)) {
+      drawHexagon(x, y);
+    }
+  }
+}
+
+function drawHexagon(x, y) {
+  ctx.beginPath();
+  for (let i = 0; i < 6; i++) {
+    ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i));
+  }
+  ctx.closePath();
+  ctx.stroke();
+}
