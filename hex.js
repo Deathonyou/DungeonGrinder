15590cc7ae36e10323bdffdb1262d946e1ca0619
Deathonyou <158500977+Deathonyou@users.noreply.github.com>
2024-04-12 10:06:07 -0500

    making one of the values 20 to test what it looks like

--- hex.js
+++ hex.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const a = 40 * Math.PI / 6;
-const r = 4;
+const r = 20;
 
 function init() {
   drawGrid(canvas.width, canvas.height);
