a7715b1b87c56d22ea6ffe588d6d1cfb8ce79318
Deathonyou <158500977+Deathonyou@users.noreply.github.com>
2024-04-12 13:12:04 -0500

    changing stuff

--- hex.js
+++ hex.js
@@ -3,27 +3,6 @@ const ctx = canvas.getContext('2d');
 
 const a = 10 * Math.PI / 30;
 const r = 50;
-
-function init() {
-init();
-drawGrid(canvas.width, canvas.height);
-}
-init();
-
-function drawGrid(width, height) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);  
-  for (let yi = r, j = 0; yi + r * Math.sin(a) < canvas.height - 20; yi += 2 ** ((j + 1) % 2) * r * Math.sin(a), j = 0) {
-    for (let xi = r; xi + r * (1 + Math.cos(a)) < canvas.width - 20; xi += r * (1 + Math.cos(a)), yi += (-1) ** j++ * r * Math.sin(a)) {
-      if (isPointInHexagon(x, y, xi, yi)) {
-        drawHexagon(xi, yi, true);
-      } else {
-        drawHexagon(xi, yi, false);
-      }
-    }
-  }
-};
-
-
 canvas.addEventListener('mousemove', function(event) {
   const rect = canvas.getBoundingClientRect();
   const x = event.clientX - rect.left;
@@ -61,4 +40,3 @@ function drawHexagon(x, y, highlighted) {
   }
   ctx.stroke();
 }
-
