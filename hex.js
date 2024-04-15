f0af0e846ae479e6a4e79b530278355cc159fef9
Deathonyou <158500977+Deathonyou@users.noreply.github.com>
2024-04-15 08:59:34 -0500

    Update hex.js

--- hex.js
+++ hex.js
@@ -1,25 +1,21 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+S(document).ready(function(){
+	var hexmap = S.hexmap('hexmap-6');
+	hexmap.positionHexes().resize();
+	hexmap.on('click',function(e){
 
-const a = 40 * Math.PI / 60;
-const r = 20;
+		S('#message-6').html('You have clicked hex '+e.i+' ('+e.hex.id+')')
 
-function init() {
-  drawGrid(canvas.width, canvas.height);
-}
-init();
-function drawGrid(width, height) {
-  for (let y = r, j = 0; y + r * Math.sin(a) < height; y += 2 ** ((j + 1) % 2) * r * Math.sin(a), j = 0) {
-    for (let x = r; x + r * (1 + Math.cos(a)) < width; x += r * (1 + Math.cos(a)), y += (-1) ** j++ * r * Math.sin(a)) {
-      drawHexagon(x, y);
-    }
-  }
-}
-function drawHexagon(x, y) {
-  ctx.beginPath();
-  for (let i = 0; i < 6; i++) {
-    ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i));
-  }
-  ctx.closePath();
-  ctx.stroke();
-}
+	}).on('mouseover',function(e){
+
+		S('#message-6').html('You have hovered over hex '+e.i+' ('+e.hex.id+')')
+
+	}).on('mouseout',function(e){
+
+		S('#message-6').html('You have left hex '+e.i+' ('+e.hex.id+')')
+
+	}).on('focus',function(e){
+
+		S('#message-6').html('You have focussed on hex '+e.i+' ('+e.hex.id+')')
+
+	});
+});
