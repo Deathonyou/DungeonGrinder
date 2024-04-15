8427557bef3eb6709b45cee4d534321632273927
Deathonyou <158500977+Deathonyou@users.noreply.github.com>
2024-04-15 08:24:47 -0500

    Update hex.js

--- hex.js
+++ hex.js
@@ -1,4 +1,25 @@
-S(document).ready(function(){
-	var hexmap = S.hexmap('hexmap-5');
-	hexmap.positionHexes().resize();
-});
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const a = 40 * Math.PI / 60;
+const r = 20;
+
+function init() {
+  drawGrid(canvas.width, canvas.height);
+}
+init();
+function drawGrid(width, height) {
+  for (let y = r, j = 0; y + r * Math.sin(a) < height; y += 2 ** ((j + 1) % 2) * r * Math.sin(a), j = 0) {
+    for (let x = r; x + r * (1 + Math.cos(a)) < width; x += r * (1 + Math.cos(a)), y += (-1) ** j++ * r * Math.sin(a)) {
+      drawHexagon(x, y);
+    }
+  }
+}
+function drawHexagon(x, y) {
+  ctx.beginPath();
+  for (let i = 0; i < 6; i++) {
+    ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i));
+  }
+  ctx.closePath();
+  ctx.stroke();
+}
